c66444418bf6c9c81cd1faf47d582ce3677e0e50
shenoyv <shenoyv@tcd.ie>
2017-12-08 20:55:05 +0000

    created  kill service functionality

--- ser.py
+++ ser.py
@@ -1,22 +1,32 @@
 import socket
 import threadpool
-import time
 import os
-
+import file_server
+# global threadpool for server
 server_thread_pool = threadpool.ThreadPool(500)
 
-port = 8018
+port_number = 1024
 
 ip_addr = socket.gethostbyname(socket.gethostname())
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock_addr = (ip_addr, port)
-print("starting up on %s port %s" % sock_addr)
-sock.bind(sock_addr)
-sock.listen(1)
-print(sock.accept())
-while True:
-    connection, client_addr = sock.accept()
-    print(sock.accept())
-    print("Connection from %s, %s\n" % connection, client_addr)
+file_manager = file_server.FileSystemManager('FileSystemDir')
+
+def create_server_socket():
+    # create socket  and initialise to localhost:8000
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_addr = ('127.0.0.1', port_number)
+    print("starting server on %s port %s" % sock_addr)
+    # bind socket to server address and wait for incoming connections4
+    sock.bind(sock_addr)
+    sock.listen(1)
+
+
+
+
+def kill_service(connection):
+    # Kill service
+    response = "Killing Service"
+    connection.sendall("%s" % response.encode())
+    connection.close()
+    os._exit(0)
+
