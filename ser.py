86b9e0208a20f1b3397a543c6349eae0e52b6e8a
shenoyv <shenoyv@tcd.ie>
2017-12-10 21:22:48 +0000

    added functionalities

--- ser.py
+++ ser.py
@@ -20,13 +20,35 @@ def create_server_socket():
     sock.bind(sock_addr)
     sock.listen(1)
 
-
-
-
 def kill_service(connection):
     # Kill service
     response = "Killing Service"
     connection.sendall("%s" % response.encode())
     connection.close()
     os._exit(0)
+
+def ls(connection, client_id, seperate_data):
+    response = ""
+    if len(seperate_data) == 1:
+        response = file_manager.list_directory_contents(client_id)
+        connection.sendall(response.encode())
+    elif len(seperate_data) == 2:
+        response = file_manager.list_directory_contents(client_id, seperate_data[1])
+        connection.sendall(response.encode())
+    else:
+        error_response(connection, 1)
+
+def cd(connection, seperate_data, client_id):
+    if len(seperate_data) == 2:
+        resp = file_manager.change_directory(seperate_data[1], client_id)
+        response = ""
+        if resp == 0:
+            response = "changed directory to %s" % seperate_data[1]
+        elif resp == 1:
+            response = "directory %s doesn't exist" % seperate_data[1]
+        connection.sendall(response.encode())
+    else:
+        error_response(connection, 1)
+
+
 
